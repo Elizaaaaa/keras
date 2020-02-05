332959e880506b5b7588975651595c1240e8f2bb
ElizaZh <elizazhang087@gmail.com>
2020-02-05 13:28:27 -0800

    Update mnist_cnn.py

--- examples/mnist_cnn.py
+++ examples/mnist_cnn.py
@@ -55,13 +55,21 @@ logger.info('initiate sequential')
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
+logger.info('add layer1')
 model.add(Conv2D(64, (3, 3), activation='relu'))
+logger.info('add layer2')
 model.add(MaxPooling2D(pool_size=(2, 2)))
+logger.info('add layer3')
 model.add(Dropout(0.25))
+logger.info('add layer4')
 model.add(Flatten())
+logger.info('add layer5')
 model.add(Dense(128, activation='relu'))
+logger.info('add layer6')
 model.add(Dropout(0.5))
+logger.info('add layer7')
 model.add(Dense(num_classes, activation='softmax'))
+logger.info('add layer8')
 
 logger.info('good with create model')
 
